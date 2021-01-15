3bcc88e175af37692fb24690f69e9a8c4d324235
Chanwoo Min <cksdn135@kookmin.ac.kr>
2021-01-15 16:34:38 +0900

    텔레그램 메시지 전송 코드 추가

--- startcamp/day3/dust.py
+++ startcamp/day3/dust.py
@@ -5,7 +5,7 @@ import requests, pprint
 
 # 2. API URL 및 KEY 값 확인
 key = 'rFJC3cDlduX33VevCCiVLHZzxbbXCkSZoZYA3ckCSM%2BGnPd%2BNnGm4tN96td9rxJuAUneJ8yAPf79bdlFPxHvzg%3D%3D'
-sidoName = '광주'
+sidoName = '서울'
 url = f'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey={key}&sidoName={sidoName}&returnType=json'
 
 # 3. 요청 및 응답값 확인
@@ -18,4 +18,13 @@ pm_10 = response['response']['body']['items'][0]['pm10Value']
 station_name = response['response']['body']['items'][0]['stationName']
 
 # 4. 최종 출력 문자열
-print(f'{sido_name}의 미세먼지 농도는 {pm_10}입니다. (측정소:{station_name})')
+text = f'{sido_name}의 미세먼지 농도는 {pm_10}입니다. (측정소:{station_name})'
+
+#5. 텔레그램 메시지 전송(sndMessage)
+token = ''
+chat_id = ''
+api_url = f'https://api.telegram.org/bot{token}'
+
+telegram_url = f'{api_url}/sendMessage?chat_id={chat_id}&text={text}'
+
+requests.get(telegram_url) 
