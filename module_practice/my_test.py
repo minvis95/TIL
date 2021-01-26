964e69114c9d61fa968126ac437a0b9a4e18f1ca
Chanwoo Min <cksdn135@kookmin.ac.kr>
2021-01-26 20:23:39 +0900

    package&module 추가

---
+++ module_practice/my_test.py
@@ -0,0 +1,13 @@
+# 테스트 파일 입니다. shift + alt + 화살표 아랠
+import check
+# from my_package.math import tools # 같은 경로 상에
+# from my_package.math.tools import my_max
+from my_package.math.tools import * # 다 들고와, 가독성 떨어져
+
+# print(dir(check))
+# print(check.odd(5))
+
+# print(dir(tools))
+# print(tools.pi)
+# print(pi)
+print(my_max(5, 4))
